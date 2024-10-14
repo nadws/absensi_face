c8f714af965a77cf16c9397871ec10748b3bd976
nadws <nandw567@gmail.com>
2024-10-14 23:22:53 +0800

    upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+        return Inertia::render('User/index', [
+            'users' => $users
+        ]);
+    }
+
+    public function create() {}
+}
