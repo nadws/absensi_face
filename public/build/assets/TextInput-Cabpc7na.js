2e65551c48044aa81cf3d4cb73bb9c4d9975406c
nadws <nandw567@gmail.com>
2024-12-24 14:34:31 +0800

    delete 39 files and update 3 files

--- public/build/assets/TextInput-Cabpc7na.js
+++
@@ -1,1 +0,0 @@
-import{r,j as a}from"./app-BQSkboee.js";const c=r.forwardRef(function({type:o="text",className:s="",isFocused:n=!1,...u},t){const e=t||r.useRef();return r.useEffect(()=>{n&&e.current.focus()},[]),a.jsx("input",{...u,type:o,className:"border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm read-only:bg-gray-500"+s,ref:e})});export{c as T};
