d7bc14a0329a4d1fb24a9636bbabc8eb86e35b5a
nadws <nandw567@gmail.com>
2025-01-04 11:07:03 +0800

    update app.css, Welcome.jsx and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
                 mona: ['"Mona Sans"', "sans-serif"],
+                poppins: ['"Poppins"', "sans-serif"],
             },
         },
     },
